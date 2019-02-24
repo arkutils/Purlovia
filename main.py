855659614d055dc6073a0609e6ce77ba1f1fb452
Ant Purchase <antoranthony.purchase@gmail.com>
2019-02-24 14:55:31 +0000

    Reformat everything
    Added yapf formatter.
    Conbined config files.
    Cleaned up issue with structex and pytest.

--- main.py
+++ main.py
@@ -2,63 +2,63 @@ from pprint import pprint
 
 from uasset import *
 
+# No content
 
 if __name__ == '__main__':
-  set_asset_path(r'.')
-  assetname = r'Game\PrimalEarth\Dinos\Dodo\Dodo_Character_BP_Aberrant'
-  # assetname = r'Game\PrimalEarth\Dinos\Dodo\Dodo_Character_BP'
+    set_asset_path(r'.')
+    assetname = r'Game\PrimalEarth\Dinos\Dodo\Dodo_Character_BP_Aberrant'
+    # assetname = r'Game\PrimalEarth\Dinos\Dodo\Dodo_Character_BP'
 
-  mem = load_asset(assetname)
-  if mem[0] != 0xC1: # This happens if you view the binary file in the editor!
-    raise ValueError(f"File corrupt again :( Got 0x{mem[0]:02X} not 0xC1")
+    mem = load_asset(assetname)
+    if mem[0] != 0xC1:  # This happens if you view the binary file in the editor!
+        raise ValueError(f"File corrupt again :( Got 0x{mem[0]:02X} not 0xC1")
 
-  asset = decode_asset(mem)
+    asset = decode_asset(mem)
 
-  # UAsset Summary
-  if True:
-    print(asset.summary)
-    print(asset.tables)
-    pprint("")
+    # UAsset Summary
+    if True:
+        print(asset.summary)
+        print(asset.tables)
+        pprint("")
 
-  # UAsset Names
-  if False:
-    pprint(asset.names)
+    # UAsset Names
+    if False:
+        pprint(asset.names)
 
-  # UAsset Imports
-  if False:
-    # Each Import Entry is 28 bytes long
-    o = asset.tables.imports_chunk.offset
-    print(f'\nImports: offset=0x{o:08X}, count={asset.tables.imports_chunk.count}')
-    for i in range(asset.tables.imports_chunk.count):
-      item = ImportTableItem(mem, o)
-      print(f'{i:2}: {item}')
-      print(asset.names[item.package], end=',')
-      print(asset.names[item.klass], end=',')
-      print(asset.names[item.name])
-      # display_mem(mem[o:o+28], as_hex_bytes, as_int32s)
-      o += 28
-    print(f'Chunk ends at offset: 0x{o:X}')
+    # UAsset Imports
+    if False:
+        # Each Import Entry is 28 bytes long
+        o = asset.tables.imports_chunk.offset
+        print(f'\nImports: offset=0x{o:08X}, count={asset.tables.imports_chunk.count}')
+        for i in range(asset.tables.imports_chunk.count):
+            item = ImportTableItem(mem, o)
+            print(f'{i:2}: {item}')
+            print(asset.names[item.package], end=',')
+            print(asset.names[item.klass], end=',')
+            print(asset.names[item.name])
+            # display_mem(mem[o:o+28], as_hex_bytes, as_int32s)
+            o += 28
+        print(f'Chunk ends at offset: 0x{o:X}')
 
-  # UAsset Exports
-  if False:
-    # Each Export Entry might be 68 bytes (comparing like words)
-    o = asset.tables.exports_chunk.offset
-    print(f'\nExports: offset=0x{o:08X}, count={asset.tables.exports_chunk.count}')
-    for i in range(asset.tables.exports_chunk.count):
-      item = ExportTableItem(mem, o)
-      print(f'{i:2}: {item}')
-      print(f'name={asset.names[item.name & 0xFFFF]}') # Note: clearing flags
+    # UAsset Exports
+    if False:
+        # Each Export Entry might be 68 bytes (comparing like words)
+        o = asset.tables.exports_chunk.offset
+        print(f'\nExports: offset=0x{o:08X}, count={asset.tables.exports_chunk.count}')
+        for i in range(asset.tables.exports_chunk.count):
+            item = ExportTableItem(mem, o)
+            print(f'{i:2}: {item}')
+            print(f'name={asset.names[item.name & 0xFFFF]}')  # Note: clearing flags
+            # display_mem(mem[o:o+24], as_hex_bytes, as_int32s)
+            # display_mem(mem[o+24:o+48], as_hex_bytes, as_int32s)
+            # display_mem(mem[o+48:o+68], as_hex_bytes, as_int32s)
+            o += 17 * 4
+        print(f'Chunk ends at offset: 0x{o:X}')
 
-      # display_mem(mem[o:o+24], as_hex_bytes, as_int32s)
-      # display_mem(mem[o+24:o+48], as_hex_bytes, as_int32s)
-      # display_mem(mem[o+48:o+68], as_hex_bytes, as_int32s)
-      o += 17*4
-    print(f'Chunk ends at offset: 0x{o:X}')
-    
-  if False:
-    # Take a look at the Depends chunk contents
-    o = asset.depends_offset
-    size = asset.header_size - o
-    print(f'\nDepends: offset=0x{o:08X}, size={size}? (up to header_size)')
-    # display_mem(mem[o:o+28], as_hex_bytes, as_int32s)
-    # display_mem(mem[o+28:o+28*2], as_hex_bytes, as_int32s)
+    if False:
+        # Take a look at the Depends chunk contents
+        o = asset.depends_offset
+        size = asset.header_size - o
+        print(f'\nDepends: offset=0x{o:08X}, size={size}? (up to header_size)')
+        # display_mem(mem[o:o+28], as_hex_bytes, as_int32s)
+        # display_mem(mem[o+28:o+28*2], as_hex_bytes, as_int32s)
--- uasset.py
+++ uasset.py
@@ -5,7 +5,6 @@ from types import SimpleNamespace as Bag
 from structex import Format, Type, Struct
 from hexutils import *
 
-
 MAX_STRING_LEN = 2048
 ENDIAN = '<'
 
@@ -13,113 +12,118 @@ asset_basepath = None
 
 
 class ImportTableItem(Struct):
-  _format = ENDIAN
-  package = Type.int64
-  klass = Type.int64
-  outer_index = Type.int32
-  name = Type.int64
+    _format = ENDIAN
+    package = Type.int64
+    klass = Type.int64
+    outer_index = Type.int32
+    name = Type.int64
+
 
 class Guid(Struct):
-  _format = ENDIAN
-  a = Type.uint32
-  b = Type.uint32
-  c = Type.uint32
-  d = Type.uint32
+    _format = ENDIAN
+    a = Type.uint32
+    b = Type.uint32
+    c = Type.uint32
+    d = Type.uint32
+
 
 class ExportTableItem(Struct):
-  _format = ENDIAN
-  klass = Type.int32
-  super = Type.int32
-  outer_index = Type.int32
-  name = Type.int64
-  object_flags = Type.uint32
-  serial_size = Type.uint32
-  serial_offset = Type.uint32
-  force_export = Type.bool32
-  not_for_client = Type.bool32
-  not_for_server = Type.bool32
-  guid = Type.Struct(Guid)
-  package_flags = Type.uint32
-  not_for_editor_game = Type.bool32
-  
+    _format = ENDIAN
+    klass = Type.int32
+    super = Type.int32
+    outer_index = Type.int32
+    name = Type.int64
+    object_flags = Type.uint32
+    serial_size = Type.uint32
+    serial_offset = Type.uint32
+    force_export = Type.bool32
+    not_for_client = Type.bool32
+    not_for_server = Type.bool32
+    guid = Type.Struct(Guid)
+    package_flags = Type.uint32
+    not_for_editor_game = Type.bool32
+
+
 class HeaderPart1(Struct):
-  _format = ENDIAN
-  tag = Type.uint32
-  legacy_ver = Type.int32
-  ue_ver = Type.int32
-  file_ver = Type.uint32
-  licensee_ver = Type.uint32
-  engine = Type.uint32
-  header_size = Type.uint32
+    _format = ENDIAN
+    tag = Type.uint32
+    legacy_ver = Type.int32
+    ue_ver = Type.int32
+    file_ver = Type.uint32
+    licensee_ver = Type.uint32
+    engine = Type.uint32
+    header_size = Type.uint32
+
 
 class ChunkPtr(Struct):
-  _format = ENDIAN
-  count = Type.uint32
-  offset = Type.uint32
+    _format = ENDIAN
+    count = Type.uint32
+    offset = Type.uint32
+
 
 class HeaderTables(Struct):
-  _format = ENDIAN
-  package_flags = Type.uint32
-  names_chunk = Type.Struct(ChunkPtr)
-  exports_chunk = Type.Struct(ChunkPtr)
-  imports_chunk = Type.Struct(ChunkPtr)
-  depends_offset = Type.uint32
-  
+    _format = ENDIAN
+    package_flags = Type.uint32
+    names_chunk = Type.Struct(ChunkPtr)
+    exports_chunk = Type.Struct(ChunkPtr)
+    imports_chunk = Type.Struct(ChunkPtr)
+    depends_offset = Type.uint32
+
 
 def set_asset_path(basepath):
-  global asset_basepath
-  asset_basepath = basepath
+    global asset_basepath
+    asset_basepath = basepath
+
 
 def convert_asset_name_to_path(name):
-  parts = name.split('\\')
-  if parts[0].lower() == 'game': parts[0] = 'Content'
-  return os.path.join(asset_basepath, *parts) + '.uasset'
+    parts = name.split('\\')
+    if parts[0].lower() == 'game': parts[0] = 'Content'
+    return os.path.join(asset_basepath, *parts) + '.uasset'
+
 
 def load_asset(name):
-  filename = convert_asset_name_to_path(name)
-  print("Loading:",filename)
-  mem = load_file_into_memory(filename)
-  return mem
-
-def load_file_into_memory(filename):
-  with open(filename, 'rb') as f:
-    data = f.read()
-    mem = memoryview(data)
+    filename = convert_asset_name_to_path(name)
+    print("Loading:", filename)
+    mem = load_file_into_memory(filename)
     return mem
 
-def parse_string(mem):
-  size, = struct.unpack('<I', mem[:4])
-  if size > MAX_STRING_LEN:
-    raise ValueError("String length invalid")
 
-  data = bytes(mem[4:4+size-1])
-  string = data.decode('utf8')
-  used = 4 + size
-  return string, used
+def parse_string(mem):
+    size, = struct.unpack('<I', mem[:4])
+    if size > MAX_STRING_LEN:
+        raise ValueError("String length invalid")
+
+    data = bytes(mem[4:4 + size - 1])
+    string = data.decode('utf8')
+    used = 4 + size
+    return string, used
+
 
 def get_chunk_count_and_offset(mem):
-  count, offset = struct.unpack_from('<II', mem[:8])
-  return Bag(count=count, offset=offset)
+    count, offset = struct.unpack_from('<II', mem[:8])
+    return Bag(count=count, offset=offset)
+
 
 def decode_header(mem):
-  asset = Bag()
-  asset.summary = HeaderPart1(mem, 0x00)
-  asset.tables = HeaderTables(mem, 0x25)
-  return asset
- 
+    asset = Bag()
+    asset.summary = HeaderPart1(mem, 0x00)
+    asset.tables = HeaderTables(mem, 0x25)
+    return asset
+
+
 def parse_names_chunk(name_chunk, mem):
-  namesArr = []
-  offset = name_chunk.offset
-  for i in range(name_chunk.count):
-    string, used = parse_string(mem[offset:])
-    offset += used
-    namesArr.append(string)
-    
-  return namesArr
+    namesArr = []
+    offset = name_chunk.offset
+    for i in range(name_chunk.count):
+        string, used = parse_string(mem[offset:])
+        offset += used
+        namesArr.append(string)
+
+    return namesArr
+
 
 def decode_asset(mem):
-  asset = decode_header(mem)
-  asset.names = parse_names_chunk(asset.tables.names_chunk, mem)
+    asset = decode_header(mem)
+    asset.names = parse_names_chunk(asset.tables.names_chunk, mem)
 
-  return asset
-  
+    return asset
