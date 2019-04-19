f8c4fd859ace7351201c6088d8c51803b229d286
Ant Purchase <antoranthony.purchase@gmail.com>
2019-04-19 23:20:34 +0100

    Extend mod exporter to batch mods together

--- exportmodvalues.py
+++ exportmodvalues.py
@@ -23,12 +23,12 @@ def error(*args):
 
 def lookup(modid, loader: AssetLoader):
     mod_name = loader.mods_numbers_to_names.get(modid)
-    return mod_name
+    long_name = loader.mods_numbers_to_descriptions.get(modid, mod_name)
+    return mod_name, long_name
 
 
-def parse(mod_name: str, loader: AssetLoader):
-    mod_number = loader.mods_names_to_numbers[mod_name]
-    log(f'Exporting {mod_number} ({mod_name})...')
+def parse(mod_name: str, modid: int, loader: AssetLoader):
+    log(f'Exporting {modid} ({mod_name})...')
 
     mod_assetnames = loader.find_assetnames(INCLUDE_REGEX, f'/Game/Mods/{mod_name}', exclude=EXCLUDE_REGEX)
 
@@ -41,9 +41,10 @@ def parse(mod_name: str, loader: AssetLoader):
     return species_data
 
 
-def convert(species_data: list):
+def convert(species_data: list, modid: str):
     values = dict()
     values['ver'] = datetime.utcnow().isoformat()
+    values['modid'] = modid
     values['species'] = list()
 
     for asset, props in species_data:
@@ -53,17 +54,42 @@ def convert(species_data: list):
     return values
 
 
-def output(mod_name: str, values: dict):
-    filename = os.path.join(args.outdir, f'{mod_name.lower()}.json')
+def output(name: str, values: dict):
+    filename = os.path.join(args.outdir, f'{name.lower()}.json')
     log(f' -> {filename}')
     with open(filename, 'w') as f:
         json.dump(values, fp=f)
 
 
+def parse_mod_args():
+    requests = [[modid.strip() for modid in field.split('+')] for field in args.mods]
+    mod_list = set(item for sublist in requests for item in sublist)
+    return requests, mod_list
+
+
+def grab_mod_info(mod_list, loader: AssetLoader):
+    mod_names = dict()
+    for modid in mod_list:
+        mod_name = loader.mods_numbers_to_names.get(modid)
+        long_name = loader.mods_numbers_to_descriptions.get(modid, mod_name)
+        mod_names[modid] = (mod_name, long_name)
+    return mod_names
+
+
+def do_convertions(requests, mod_names, loader: AssetLoader):
+    for request in requests:
+        modids = '+'.join(str(modid) for modid in request)
+        name = '+'.join(mod_names[modid][1] for modid in request)
+        species_data = sum((parse(mod_names[modid][0], modid, loader) for modid in request), [])
+        values = convert(species_data, modids)
+        output(name, values)
+
+
 def create_parser():
     import argparse
-    parser = argparse.ArgumentParser(description="Export mod data for ASB in values.json format")
-    parser.add_argument('mods', metavar='MODID', type=str, nargs='+', help='ID of a mod to export (can appear multiple times)')
+    parser = argparse.ArgumentParser(description="Export mod data for ASB in values.json format.")
+    parser.add_argument(
+        'mods', metavar='MODID', type=str, nargs='+', help='ID of a mod to export (can appear multiple times, use + to combine)')
     parser.add_argument('-o', dest='outdir', default='mods', help='directory to save the output(s) into (default: mods)')
     parser.add_argument(
         '-c', dest='createdir', action='store_const', const=True, default=False, help='create \'outdir\' if it doesn\'t exist')
@@ -93,14 +119,9 @@ def main():
 
     loader = AssetLoader(quiet=args.quiet)
 
-    for modid in args.mods:
-        mod_name = lookup(modid, loader) or error(f"Unable to find details for mod {modid}")
-        mod_names[modid] = mod_name
-
-    for (modid, mod_name) in mod_names.items():
-        species_data = parse(mod_name, loader)
-        values = convert(species_data)
-        output(mod_name, values)
+    requests, mod_list = parse_mod_args()
+    mod_names = grab_mod_info(mod_list, loader)
+    do_convertions(requests, mod_names, loader)
 
 
 if __name__ == '__main__':
