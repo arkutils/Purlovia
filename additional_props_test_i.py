7e43afc0e2ac026480bf5cb3c20aee43e0dd3ecc
VolatilePulse <shearer.zachary@gmail.com>
2019-04-23 22:53:18 -0400

    Add a testing method for creature color and region names
    Attempted to match the values.json as close as possible.
    ColorID still needs to have the names converted to IDs.
    Still need to convert species specific region names to match values.json currently.

--- additional_props_test_i.py
+++ additional_props_test_i.py
@@ -23,177 +23,71 @@ loader = AssetLoader()
 character_asset = loader[assetname]
 character_props = ark.mod.gather_properties(character_asset)
 
-#%% Get the colorsets for both genders
-# character_props['ColorizationIntensity'][0][-1]
-# character_props['bUseColorization'][0][-1]
+default_color_regions = [0, 0, 0, 0, 0, 0]
 
-male_colorset_asset = loader.load_related(character_props['RandomColorSetsMale'][0][-1])
-# female_colorset_asset = loader.load_related(character_props['RandomColorSetsFemale'][0][-1])
+if str(character_props['bUseColorization'][0][-1]) is 'False':
+    print('No Color Regions Used')
+else:
+    male_colorset_asset = loader.load_related(character_props['RandomColorSetsMale'][0][-1])
+    male_colorset_props = ark.mod.gather_properties(male_colorset_asset)
 
-male_colorset_props = ark.mod.gather_properties(male_colorset_asset)
-# female_colorset_props = ark.mod.gather_properties(female_colorset_asset)
+    # Female Colorset
+    # female_colorset_asset = loader.load_related(character_props['RandomColorSetsFemale'][0][-1])
+    # female_colorset_props = ark.mod.gather_properties(female_colorset_asset)
 
-male_colorset_props['ColorSetDefinitions'][0][-1]
-# female_colorset_props['ColorSetDefinitions'][0][-1]
+    # Reference from ASB's values.json file
+    # "colors":[{"name":"Body Main","colorIds":[25,28,8,51,52,30,36,38,48,50,56]}
+    # {"name":null,"colorIds":[]}
+    colors = []
+    for i, region in enumerate(default_color_regions):
+        prevent_region = stat_value(character_props, 'PreventColorizationRegions', i, region)
+        color = dict()
+        color_names = []
+
+        if prevent_region:
+            color['name'] = 'null'
+        else:
+            color_set_defs = male_colorset_props['ColorSetDefinitions'][i][-1]
+
+            # Color Set has a Region Name
+            if str(color_set_defs.values[0].name) == 'RegionName':
+                color['name'] = str(color_set_defs.values[0].value)
+            else:
+                color['name'] = 'No Name Available'
+
+            if str(color_set_defs.values[-1].name) == 'ColorEntryNames':
+                for color_name in color_set_defs.values[-1].value.values:
+                    if str(color_name) not in color_names:
+                        color_names.append(str(color_name))
+
+        color['colorIds'] = color_names
+        colors.append(color)
+
+    pprint(colors)
 
 #%% Get the taming data for the species
-print('\nTaming:\n')
-print(f'requiredTameAffinity: {stat_value(character_props, "RequiredTameAffinity", 0)}')
-print(f'requiredtameaffinityperbaselevel: {stat_value(character_props, "RequiredTameAffinityPerBaseLevel", 0)}')
-print(f'tameIneffectivenessByAffinity: {stat_value(character_props, "TameIneffectivenessByAffinity", 0)}')
-print(
-    f'tamingIneffectivenessModifierIncreaseByDamagePercent: {stat_value(character_props, "TamingIneffectivenessModifierIncreaseByDamagePercent", 0)}'
-)
-print(f'damageTypeAdjusters: {pretty(character_props["DamageTypeAdjusters"][0][-1])}')
+# print('\nTaming:\n')
+# print(f'requiredTameAffinity: {stat_value(character_props, "RequiredTameAffinity", 0)}')
+# print(f'requiredtameaffinityperbaselevel: {stat_value(character_props, "RequiredTameAffinityPerBaseLevel", 0)}')
+# print(f'tameIneffectivenessByAffinity: {stat_value(character_props, "TameIneffectivenessByAffinity", 0)}')
+# print(
+#     f'tamingIneffectivenessModifierIncreaseByDamagePercent: {stat_value(character_props, "TamingIneffectivenessModifierIncreaseByDamagePercent", 0)}'
+# )
+# print(f'damageTypeAdjusters: {pretty(character_props["DamageTypeAdjusters"][0][-1])}')
 
-#%% Get the breeding data for the species
-# Determines if a species is breedable (most likely anyways) :shrug:
-# print(character_props['bCanHaveBaby'][0][-1])
+# dino_settings_asset = loader.load_related(character_props["DinoSettingsClass"][0][-1])
+# dino_settings_props = ark.mod.gather_properties(dino_settings_asset)
 
-# if this, 'incubationTime' = 0; else gestationTime = 0
-incubation_time = 0
-gestation_time = 0
-fert_egg_props = None
-if character_props['bUseBabyGestation']:
-    # 'gestationTime' = 1 / (Baby Gestation Speed × Extra Baby Gestation Speed Multiplie)
-    # gestation_speed = character_props['BabyGestationSpeed'][0][-1].value
-    # READ ME!!! DevKit mentions this properties name is BITBREAK. It shares that name with
-    #   several other properties, presumably a property only found in the binary. I haven't
-    #   looked at much more than the properties thus far, but definitely worth a dig as
-    #   it isn't showing up in the properties inherited, but there are other properties
-    #   that we need to add to the file as well.
-    # Values verified through DevKit, not the binary
-
-    gestation_speed = stat_value(character_props, 'BabyGestationSpeed', 0, 0.000035)
-    extra_gestation_speed_m = stat_value(character_props, 'ExtraBabyGestationSpeedMultiplier', 0, 1.0)
-    gestation_time = 1 / gestation_speed / extra_gestation_speed_m
-
-    print('\nGestation:\n')
-
-elif character_props['FertilizedEggItemsToSpawn'][0]:
-    # In the blueprint of the fertilized egg PrimalItemConsumable_Egg_…_Fertilized:
-    # Usually an array(?) but ASB only supports 1 fertilized egg
-    fert_egg_asset = loader.load_related(character_props['FertilizedEggItemsToSpawn'][0][-1].values[0])
-    fert_egg_props = ark.mod.gather_properties(fert_egg_asset)
-
-    # 'incubationTime' = 100 / (Egg Lose Durability Per Second × Extra Egg Lose Durability Per Second Multiplier)
-    egg_decay = stat_value(fert_egg_props, 'EggLoseDurabilityPerSecond', 0)
-    extra_egg_decay_m = stat_value(fert_egg_props, 'ExtraEggLoseDurabilityPerSecondMultiplier', 0)
-    incubation_time = 100 / egg_decay / extra_egg_decay_m
-
-    print('\nEggs:\n')
-    min_temp = stat_value(fert_egg_props, 'EggMinTemperature', 0)
-    print(f'eggTempMin: {min_temp}')
-    max_temp = stat_value(fert_egg_props, 'EggMaxTemperature', 0)
-    print(f'eggTempMax: {max_temp}')
-
-print(f'gestationTime: {gestation_time}')
-print(f'incubationTime: {incubation_time}')
-
-print('\nRaising:\n')
-# 'maturationTime' = 1 / (Baby Age Speed × Extra Baby Age Speed Multiplier)
-baby_age_speed = stat_value(character_props, 'BabyAgeSpeed', 0)
-extra_baby_age_speed_m = stat_value(character_props, 'ExtraBabyAgeSpeedMultiplier', 0)
-mature_time = 1 / baby_age_speed / extra_baby_age_speed_m
-print(f'maturationTime: {mature_time}')
-
-# Not included in most Character assets (inherited from the Primal Dino Character asset)
-# 'matingCooldownMin' (default: 64800.0)
-min_mating_cooldown = stat_value(character_props, 'NewFemaleMinTimeBetweenMating', 0, 64800.0)
-print(f'matingCooldownMin: {min_mating_cooldown}')
-
-# Not included in most Character assets (inherited from the Primal Dino Character asset)
-# 'matingCooldownMax' (default: 172800.0)
-max_mating_cooldown = stat_value(character_props, 'NewFemaleMaxTimeBetweenMating', 0, 172800.0)
-print(f'matingCooldownMax: {max_mating_cooldown}')
-
-###############################
-# Seemingly irrelevant properties
-###############################
-# Baby's size compared to parent
-# print(character_props['BabyScale'][0][-1].value)
-
-# Might be related to recent imprinting overhaul and just coincidence it's close to maturity time
-# print(character_props['BabyImprintingQualityTotalMaturationTime'][0][-1].value)
-
-# Appears to be a time to mate during mating session (2 minutes of mating)
-# print(character_props['FemaleMatingTime'][0][-1].value)
-
-# Baby's relative movement speed (too slow, Wildcard!)
-# print(character_props['BabySpeedMultiplier'][0][-1].value)
-
-# How fast it is ejected from the womb?
-# print(character_props['BabyPitchMultiplier'][0][-1].value)
-# How annoyingly loud the baby's sounds are?
-# print(character_props['BabyVolumeMultiplier'][0][-1].value)
-
-# I know, I know, just trying to have some fun while I die of banging my head
-#   against my keyboard. Just left it for you to chuckle about
-
-###############################
-# Data Dumps
-###############################
-# values.json - Polar Bear
-# "breeding":{
-#     "gestationTime":14285.713867,
-#     "incubationTime":0,
-#     "maturationTime":166666.65625,
-#     "matingCooldownMin":64800,
-#     "matingCooldownMax":172800
-# }
-
-# ark-data - Dire Bear
-# "breeding":{
-#     "gestationTime":14285.713867,
-#     "maturationTime":166666.65625,
-#     "babyTime":16666.666016,
-#     "juvenileTime":66666.664063,
-#     "adolescentTime":83333.328125,
-#     "minTimeBetweenMating":64800,
-#     "maxTimeBetweenMating":172800
-# },
-
-# ark-data for Pteranodon
-# "breeding":{
-#     "eggs":[
-#         {
-#             "minTemp":29,
-#             "maxTemp":32,
-#             "incubationTime":5999.520508
-#         }
-#     ],
-#     "maturationTime":133333.328125,
-#     "babyTime":13333.333008,
-#     "juvenileTime":53333.332031,
-#     "adolescentTime":66666.664063,
-#     "minTimeBetweenMating":64800,
-#     "maxTimeBetweenMating":172800
-# },
-
-# ark-data for Gigant
-# "breeding":{
-#     "eggs":[
-#         {
-#             "minTemp":43,
-#             "maxTemp":44,
-#             "incubationTime":179985.609375
-#         }
-#     ],
-#     "maturationTime":1010100.875,
-#     "babyTime":101010.085938,
-#     "juvenileTime":404040.34375,
-#     "adolescentTime":505050.4375,
-#     "minTimeBetweenMating":64800,
-#     "maxTimeBetweenMating":172800
-# },
-
-# values.json - Ice Wyvern
-# "breeding":{
-#     "gestationTime":0,
-#     "incubationTime":17998.560547,
-#     "maturationTime":333333.3125,
-#     "matingCooldownMin":64800,
-#     "matingCooldownMax":172800,
-#     "eggTempMin":80,
-#     "eggTempMax":90
-# },
+# taming_foods = []
+# for food in dino_settings_props["FoodEffectivenessMultipliers"][0][-1].values:
+#     food["FoodEffectivenessMultiplier"]
+#     food["HealthEffectivenessMultiplier"]
+#     food["TorpidityEffectivenessMultiplier"]
+#     food["AffinityEffectivenessMultiplier"]
+#     food["AffinityOverride"] # second d entry
+#     food["StaminaEffectivenessMultiplier"]
+#     food["FoodItemCategory"]
+#     food["FoodItemParent"] # asset for actual food
+#     food["UntamedFoodConsumptionPriority"]
+#     for value in food.values:
+#         print(f'Name: {value.name}\t\t Value: {value.value}')
