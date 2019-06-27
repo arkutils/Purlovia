97c6f3fe627d133e71f375da9a65fe690fe0cda7
VolatilePulse <shearer.zachary@gmail.com>
2019-06-27 01:30:29 -0400

    Check all available assets for "valid" species
    Added a performance timer to evaluate the speed when adding more mods
    Cleaned up some hacky code to be more efficient and readable

--- asset_testing_i.py
+++ asset_testing_i.py
@@ -1,6 +1,7 @@
 #%% Run Me
 import json
 import os
+from time import process_time, perf_counter
 from automate.ark import ArkSteamManager, readModData
 
 arkman = ArkSteamManager('./livedata', skipInstall=True)
@@ -11,71 +12,59 @@ assets_to_load = []
 with open('asb_json/values.json') as json_file:
     data = json.load(json_file)
     for species in data['species']:
-        assets_to_load.append(species['blueprintPath'])
+        assets_to_load.append(species['blueprintPath'].split('.')[0])
 for r, d, f in os.walk('./output'):
     for file in f:
         with open(os.path.join(r, file)) as json_file:
             data = json.load(json_file)
             for species in data['species']:
-                assets_to_load.append(species['blueprintPath'])
+                assets_to_load.append(species['blueprintPath'].split('.')[0])
 
 print(len(assets_to_load))
 
 #%% Extract Names from assets and compare them
-real_names = []
-names = []
+real_names = {}
+names = {}
 first_run = True
 skipped_count = 0
-for asset_to_load in assets_to_load:
-    asset = loader[asset_to_load.split('.')[0]]
+for asset in assets_to_load:
+    names = set(str(name) for name in loader.partially_load_asset(asset).names)
     if first_run:
-        for name in asset.names.values:
-            real_names.append(name.value)
+        first_run = False
+        real_names = names
         print(real_names)
     else:
-        for name in asset.names.values:
-            names.append(name.value)
         initial_len = len(real_names)
-        real_names = list(set(names) & set(real_names))
+        real_names = names & real_names
         if initial_len > len(real_names):
             print(f'Parsed {skipped_count} without removing any names')
             skipped_count = 0
+            print(asset)
             print(f'Removed {initial_len - len(real_names)} names')
         else:
             skipped_count += 1
 
         # print(real_names)
 
-    if first_run:
-        first_run = False
-
-    # input("Skip...")
-
 print(f'Parsed {skipped_count} without removing any names')
 print(real_names)
 
 #%% Attempt to load assets and verify if they're a species asset or not
-required_names = [
-    'DescriptiveName', 'CapsuleComponent', 'DefaultSceneRootNode', 'CharMoveComp', 'ShooterCharacterMovement',
-    'CollisionCylinder', 'UpdatedComponent', 'SkeletalMesh', 'AttachParent', 'CategoryName', 'CharacterMesh0', 'Game',
-    'MaterialInstanceConstant', 'SkeletalMeshComponent', 'RootComponent', 'Default', 'CharacterMovement',
-    'SimpleConstructionScript', 'VariableName', 'UserConstructionScript', 'SCS_Node', 'bUseColorization', 'ComponentTemplate',
-    'Mesh', 'DefaultSceneRoot', 'SceneComponent'
-]
+names = {}
+valid_assets = set()
+num_assets = 0
 
-names = []
+start_time1 = process_time()
+start_time2 = perf_counter()
+for assetname in loader.find_assetnames('.*', '/Game'):
+    num_assets += 1
+    names = set(str(name) for name in loader.partially_load_asset(assetname).names)
+    if 'ShooterCharacterMovement' in names:
+        valid_assets.add(assetname)
+end_time1 = process_time()
+end_time2 = perf_counter()
 
-asset = loader['/Game/Mods/ClassicFlyers/Dinos/AdminArgent/ClassicAdminArgent_Character_BP']
-asset = loader['/Game/Mods/ClassicFlyers/Dinos/AdminArgent/DinoCharacterStatusComponent_BP_ClassicAdminArgent']
-asset = loader['/Game/Mods/ClassicFlyers/Dinos/AdminArgent/DinoCharacterStatusComponent_BP_ClassicAdminArgent']
-asset = loader['/Game/Mods/ClassicFlyers/Dinos/AdminArgent/DinoCharacterStatusComponent_BP_ClassicAdminArgent']
-asset = loader['/Game/Mods/ClassicFlyers/Dinos/AdminArgent/DinoCharacterStatusComponent_BP_ClassicAdminArgent']
-asset = loader['/Game/Mods/ClassicFlyers/Dinos/AdminArgent/Assets/DinoColorSet_AdminArgent']
-
-for name in asset.names.values:
-    names.append(name.value)
-
-output = list(set(required_names) - set(names))
-print(output)
-
-#%%
+print(f'process_time: {end_time1 - start_time1} seconds')
+print(f'perf_counter: {end_time2 - start_time2} seconds')
+print(len(valid_assets))
+print(num_assets)
