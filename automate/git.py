0b4e1baf9cbd1e7f5d076db6d3a365aa5301d559
coldino <antoranthony.purchase@gmail.com>
2019-07-09 02:21:56 +0100

    Extend git placeholder module with plan/comments

--- automate/git.py
+++ automate/git.py
@@ -10,7 +10,15 @@ class GitManager:
         self.config = config or get_global_config()
 
     def before_exports(self):
+        # Validate clone is valid
+        # Check branch is correct (maybe checkout if not?)
+        # Pull and ensure it was clean
         pass
 
     def after_exports(self):
+        # If no files changed, return
+        # Add all changed files
+        # Construct commit message using a simple message plus names and versions of all changed files.
+        # Commit
+        # Push
         pass
