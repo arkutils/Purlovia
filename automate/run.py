a544d4d42c33cd0c9d560a420c543f629b28143f
coldino <antoranthony.purchase@gmail.com>
2019-07-13 14:52:26 +0100

    Fix issue with `yapf` in `automate` directory
    The way VSCode launches `yapf` means it fails if there are files present in the same directory with names matching Python build-in modules.
    
    Workaround in this case is to move the single function out of `logging.py`.

--- automate/run.py
+++ automate/run.py
@@ -1,19 +1,40 @@
+import os
 import logging
+import logging.config
 import argparse
 from typing import *
 from pathlib import Path
+
+import yaml
 
 from config import get_global_config, ConfigFile
 
 from .git import GitManager
 from .ark import ArkSteamManager
 from .export import export_values
-from .logging import setup_logging
 
 # pylint: enable=invalid-name
 
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 logger.addHandler(logging.NullHandler())
+
+
+def setup_logging(path='config/logging.yaml', level=logging.INFO):
+    '''Setup logging configuration.'''
+    if os.path.exists(path):
+        with open(path, 'rt') as f:
+            config = yaml.safe_load(f.read())
+        logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=level)
+
+    logging.captureWarnings(True)
+
+    root_logger = logging.getLogger()
+    logging.addLevelName(100, 'STARTUP')
+    root_logger.log(100, '')
+    root_logger.log(100, '-' * 100)
+    root_logger.log(100, '')
 
 
 def get_level_names():
