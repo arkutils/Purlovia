1068196ab5b4efeb36c61bb076c10218e2e387f8
Dustin Pianalto <dustin@djpianalto.com>
2019-07-31 23:17:27 -0800

    Update document path for Sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
@@ -27,7 +27,11 @@ author = 'coldino'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+    'sphinx.ext.napoleon',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -49,3 +53,6 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+# -- Extension configuration -------------------------------------------------
--- docs/index.rst
+++ docs/index.rst
@@ -1,17 +1,19 @@
 .. Purlovia documentation master file, created by
-   sphinx-quickstart on Wed Jul 31 14:37:22 2019.
+   sphinx-quickstart on Wed Jul 31 21:26:28 2019.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
 Welcome to Purlovia's documentation!
 ====================================
 
+.. automodule:: gui.tree
+   :members:
+
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
-.. autoclass:: gui.tree
-   :members:
+
 
 Indices and tables
 ==================
