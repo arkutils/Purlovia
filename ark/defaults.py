8ead23d718d1e6d9d090db2899d1a1c7b57ba660
coldino <antoranthony.purchase@gmail.com>
2019-08-04 01:57:50 +0100

    Add example proxy structures for Ark

--- ark/defaults.py
+++ ark/defaults.py
@@ -1,3 +1,5 @@
+from ue.proxy import *
+
 FEMALE_MINTIMEBETWEENMATING_DEFAULT = 64800.0
 FEMALE_MAXTIMEBETWEENMATING_DEFAULT = 172800.0
 
@@ -12,3 +14,13 @@ CANLEVELUP_VALUES = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
 
 # Stats that are represented as percentages instead
 IS_PERCENT_STAT = (0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1)
+
+
+class DCSC(UEProxyStructure, uetype='/Script/ShooterGame.PrimalDinoStatusComponent'):
+    MaxStatusValues = uefloats(100.0, 100.0, 0.0, 100.0, 0, '00000000')
+    CanLevelUp = uebools(True, False, True, False)
+
+
+class DinoChar(UEProxyStructure, uetype='/Script/ShooterGame.PrimalDinoCharacter'):
+    # DescriptiveName = uestrings('') # need uestrings!
+    Something = uefloats(0.0, 0.0)
