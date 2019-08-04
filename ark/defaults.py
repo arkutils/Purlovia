ed40c81bd7119d75a70e70e7436f636e4b4f6332
coldino <antoranthony.purchase@gmail.com>
2019-08-04 16:08:26 +0100

    Fill in defaults for DCSC and dino char proxies

--- ark/defaults.py
+++ ark/defaults.py
@@ -17,10 +17,62 @@ IS_PERCENT_STAT = (0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1)
 
 
 class DCSC(UEProxyStructure, uetype='/Script/ShooterGame.PrimalDinoStatusComponent'):
-    MaxStatusValues = uefloats(100.0, 100.0, 0.0, 100.0, 0, '00000000')
-    CanLevelUp = uebools(True, False, True, False)
+    AmountMaxGainedPerLevelUpValue = uefloats(0, 0, 0.06, 0, 0, 0, 0, 0, 0, 0, 0, 0)  # TODO: Should 0.06 be here? needs raw data
+    AmountMaxGainedPerLevelUpValueTamed = uefloats(*(0, ) * 12)
+    BaseFoodConsumptionRate = uefloats(-0.025000)  # TODO: needs raw data
+    CanLevelUpValue = uebytes(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+    DontUseValue = uebytes(0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1)
+    ExtraTamedHealthMultiplier = uefloats((1.35, 'CDCCAC3F'), 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
+    MaxStatusValues = uefloats(100, 100, 100, 100, 100, 100, 0, 0, 0, 0, 0, 0)
+    ProneWaterFoodConsumptionMultiplier = uefloats(1.0)
+    RecoveryRateStatusValue = uefloats(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)  # TODO: Need numbers - only one known is 0 for torpor
+    TamingMaxStatAdditions = uefloats(*(0, ) * 12)
+    TamingMaxStatMultipliers = uefloats(*(0, ) * 12)
 
 
 class DinoChar(UEProxyStructure, uetype='/Script/ShooterGame.PrimalDinoCharacter'):
-    # DescriptiveName = uestrings('') # need uestrings!
-    Something = uefloats(0.0, 0.0)
+    BabyAgeSpeed = uefloats((0.033, '022B073D'))
+    BabyGestationSpeed = uefloats((0.000035, 'F7CC1238'))
+    bCanBeTamed = uebools(True)
+    bCanBeTorpid = uebools(True)
+    bIgnoreAllImmobilizationTraps = uebools(False)
+    bIsBossDino = uebools(False)
+    bIsCorrupted = uebools(False)
+    bIsWaterDino = uebools(False)
+    bPreventImmobilization = uebools(False)
+    bPreventSleepingTame = uebools(False)
+    bSupportWakingTame = uebools(False)
+    bUseBabyGestation = uebools(False)
+    CustomTag = uestrings('')
+    DescriptiveName = uestrings('')
+    DragWeight = uefloats(35.0)
+    ExtraBabyAgeSpeedMultiplier = uefloats(1.0)
+    ExtraBabyGestationSpeedMultiplier = uefloats(1.0)
+    KnockedOutTorpidityRecoveryRateMultiplier = uefloats(3.0)
+    Mass = uefloats(100.0)
+    NewFemaleMaxTimeBetweenMating = uefloats(172800.0)
+    NewFemaleMinTimeBetweenMating = uefloats(64800.0)
+    PreventColorizationRegions = uebytes(0, 0, 0, 0, 0, 0)
+    RequiredTameAffinity = uefloats(100)
+    RequiredTameAffinityPerBaseLevel = uefloats(5.0)
+    TameIneffectivenessByAffinity = uefloats(20)
+    TheMaxTorporIncreasePerBaseLevel = uefloats(0.06)  # TODO: needs raw data
+    WakingTameFoodAffinityMultiplier = uefloats(1.6)  # TODO: needs raw data
+    WakingTameFoodConsumptionRateMultiplier = uefloats(2.0)
+
+    # TODO: Other types, not yet supported
+    # RandomColorSetsMale
+    # RandomColorSetsFemale
+    # FertilizedEggItemsToSpawn
+    # BoneDamageAdjusters
+
+
+# PGD
+#   ColorDefinitions
+#   MasterDyeList
+
+# Egg
+#   EggLoseDurabilityPerSecond
+#   ExtraEggLoseDurabilityPerSecondMultiplier
+#   EggMinTemperature
+#   EggMaxTemperature
