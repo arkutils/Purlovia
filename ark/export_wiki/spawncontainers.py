91fe4eb83a7983cf966d8ac6ea49acb86b820e56
alex4401 <rylatgl@gmail.com>
2019-09-21 11:13:17 +0200

    Refactor spawn group extraction to let other scripts create the objects
    Also includes "Move chance calculation out of the gatherer"

--- ark/export_wiki/spawncontainers.py
+++ ark/export_wiki/spawncontainers.py
@@ -1,10 +1,11 @@
-from dataclasses import dataclass, field
+from dataclasses import InitVar, dataclass, field
 from logging import NullHandler, getLogger
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Union
 
 from ue.asset import UAsset
 from ue.loader import AssetLoader, AssetNotFound
-from ue.properties import ArrayProperty, StringProperty
+from ue.properties import (ArrayProperty, FloatProperty, StringProperty,
+                           StructProperty)
 
 logger = getLogger(__name__)
 logger.addHandler(NullHandler())
@@ -12,20 +13,32 @@ logger.addHandler(NullHandler())
 
 @dataclass
 class SpawnGroupEntry:
-    name: StringProperty
-    chance: float = 0.0
-    weight: float = 1.0
+    _struct: InitVar[StructProperty]
+
+    name: StringProperty = field(init=False)
+    chance: Union[float, FloatProperty] = 0.0
+    weight: Union[float, FloatProperty] = 1.0
     npcsToSpawn: List[str] = field(default_factory=lambda: [])
     npcsSpawnOffsets: List[Dict[str,Any]] = field(default_factory=lambda: [])
     npcsToSpawnPercentageChance: List[float] = field(default_factory=lambda: [])
     npcMinLevelOffset: List[float] = field(default_factory=lambda: [])
     npcMaxLevelOffset: List[float] = field(default_factory=lambda: [])
 
+    def __post_init__(self, _struct):
+        data = _struct.as_dict()
+        self.name = data['AnEntryName']
+        self.weight = data['EntryWeight']
+        self.npcsToSpawn = data['NPCsToSpawn']
+        self.npcsSpawnOffsets = [{'x': offset.x.value, 'y': offset.y.value, 'z': offset.z.value} for offset in data['NPCsSpawnOffsets'].values]
+        self.npcsToSpawnPercentageChance = data['NPCsToSpawnPercentageChance']
+        self.npcMinLevelOffset = data['NPCMinLevelOffset']
+        self.npcMaxLevelOffset = data['NPCMaxLevelOffset']
+
     def format_for_json(self):
         data = {}
         for field_name in self.__annotations__: # pylint:disable=E1101
-            field_value = getattr(self, field_name)
-            if not field_value or isinstance(field_value, ArrayProperty) and not field_value.values:
+            field_value = getattr(self, field_name, None)
+            if not field_value:
                 continue
             data[field_name] = field_value
         return data
@@ -33,8 +46,15 @@ class SpawnGroupEntry:
 
 @dataclass
 class SpawnGroupLimitEntry:
-    npcClass: str
+    _struct: InitVar[StructProperty]
+
+    npcClass: str = field(init=False)
     maxPercentageOfDesiredNumToAllow: float = 1.0
+
+    def __post_init__(self, _struct):
+        data = _struct.as_dict()
+        self.npcClass = data['NPCClass']
+        self.maxPercentageOfDesiredNumToAllow = getattr(data, 'MaxDesiredNumEnemiesMultiplier', 1.0)
 
     def format_for_json(self):
         return {'path': self.npcClass, 'maxPercentageOfDesiredNumToAllow': self.maxPercentageOfDesiredNumToAllow}
@@ -47,6 +67,17 @@ class SpawnGroupObject:
     entries: List[SpawnGroupEntry] = field(default_factory=lambda: [])
     limits: List[SpawnGroupLimitEntry] = field(default_factory=lambda: [])
 
+    def calculate_chances(self):
+        weight_sum = sum([entry.weight for entry in self.entries])
+        
+        if not weight_sum:
+            #logger.debug(f'Sum of entry weights in {self.blueprintPath} is zero.')
+            return
+
+        for entry in self.entries:
+            entry.chance = entry.weight / weight_sum
+        self.entries.sort(key=lambda entry: entry.chance, reverse=True)
+
     def as_dict(self):
         return {
             "path": self.blueprintPath,
@@ -54,6 +85,9 @@ class SpawnGroupObject:
             "entries": self.entries,
             "limits": self.limits
         }
+    
+    def format_for_json(self):
+        return self.as_dict()
 
 
 def gather_spawn_entries(asset: UAsset):
@@ -65,16 +99,7 @@ def gather_spawn_entries(asset: UAsset):
         return
 
     for entry in entries[0].values:
-        entry_data = entry.as_dict()
-        entry_object = SpawnGroupEntry(entry_data['AnEntryName'])
-        entry_object.weight = entry_data['EntryWeight']
-        entry_object.npcsToSpawn = entry_data['NPCsToSpawn']
-        entry_object.npcsSpawnOffsets = [{'x': offset.x.value, 'y': offset.y.value, 'z': offset.z.value}
-                                           for offset in entry_data['NPCsSpawnOffsets'].values]
-        entry_object.npcsToSpawnPercentageChance = entry_data['NPCsToSpawnPercentageChance']
-        entry_object.npcMinLevelOffset = entry_data['NPCMinLevelOffset']
-        entry_object.npcMaxLevelOffset = entry_data['NPCMaxLevelOffset']
-        yield entry_object
+        yield SpawnGroupEntry(entry)
 
 
 def gather_limit_entries(asset: UAsset):
@@ -86,11 +111,7 @@ def gather_limit_entries(asset: UAsset):
         return
 
     for entry in entries[0].values:
-        entry_data = entry.as_dict()
-        entry_object = SpawnGroupLimitEntry(entry_data['NPCClass'])
-        if 'MaxDesiredNumEnemiesMultiplier' in entry_data:
-            entry_object.maxPercentageOfDesiredNumToAllow = entry_data['MaxDesiredNumEnemiesMultiplier'].value
-        yield entry_object
+        yield SpawnGroupLimitEntry(entry)
 
 
 def get_spawn_entry_container_data(loader: AssetLoader, full_asset_name: str) -> Optional[SpawnGroupObject]:
@@ -113,9 +134,6 @@ def get_spawn_entry_container_data(loader: AssetLoader, full_asset_name: str) ->
     limits = list(gather_limit_entries(container_data))
     #del loader[asset_name]
 
-    weight_sum = sum([entry.weight for entry in entries])
-    for entry in entries:
-        entry.chance = entry.weight / weight_sum
-    entries.sort(key=lambda entry: entry.chance, reverse=True)
-
-    return SpawnGroupObject(full_asset_name, max_desired_enemy_num_mult, entries, limits)
+    group_object = SpawnGroupObject(full_asset_name, max_desired_enemy_num_mult, entries, limits)
+    group_object.calculate_chances()
+    return group_object
