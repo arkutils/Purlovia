59926bc470f4ea10bbf4d14c812aa924ed23b2f1
coldino <antoranthony.purchase@gmail.com>
2019-11-17 16:10:45 +0000

    Add GitHub Actions Workflow for CI
    Runs mypy and pytest after each push.

--- tests/test_context.py
+++ tests/test_context.py
@@ -1,6 +1,9 @@
+import pytest  # type: ignore
+
 from .common import *
 
 
+@pytest.mark.requires_game
 def test_defaults(loader: AssetLoader):
     loader.wipe_cache()
     asset = loader[ASSETNAME]
@@ -16,6 +19,7 @@ def test_defaults(loader: AssetLoader):
         assert not asset.has_bulk_data
 
 
+@pytest.mark.requires_game
 def test_linking(loader: AssetLoader):
     loader.wipe_cache()
     with ue_parsing_context(link=False):
@@ -33,6 +37,7 @@ def test_linking(loader: AssetLoader):
         assert asset.is_linked
 
 
+@pytest.mark.requires_game
 def test_properties(loader: AssetLoader):
     loader.wipe_cache()
     with ue_parsing_context(properties=False):
@@ -50,6 +55,7 @@ def test_properties(loader: AssetLoader):
         assert asset.has_properties
 
 
+@pytest.mark.requires_game
 def test_no_properties_without_link(loader: AssetLoader):
     loader.wipe_cache()
     with ue_parsing_context(link=False, properties=True):
@@ -57,6 +63,7 @@ def test_no_properties_without_link(loader: AssetLoader):
         assert not asset.has_properties
 
 
+@pytest.mark.requires_game
 def test_bulk_data(loader: AssetLoader):
     loader.wipe_cache()
     with ue_parsing_context(bulk_data=False):
