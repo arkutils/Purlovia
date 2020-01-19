4553d94f5ac9cc53bb2b2636f784381e4fafa962
coldino <antoranthony.purchase@gmail.com>
2020-01-19 12:50:12 +0000

    Add status effects to item export

--- export/wiki/stage_items.py
+++ export/wiki/stage_items.py
@@ -57,29 +57,25 @@ class ItemsStage(JsonHierarchyExportStage):
         v['maxQuantity'] = item.MaxItemQuantity[0]
 
         if item.has_override('SpoilingTime'):
-            v['spoilage'] = dict(
-                time=item.SpoilingTime[0]
-            )
+            v['spoilage'] = dict(time=item.SpoilingTime[0])
             if item.has_override('SpoilingItem'):
                 v['spoilage']['productBP'] = item.SpoilingItem[0]
 
         if item.bUseItemDurability[0].value:
-            v['durability'] = dict(
-                min=item.MinItemDurability[0],
-                ignoreInWater=item.bDurabilityRequirementIgnoredInWater[0]
-            )
+            v['durability'] = dict(min=item.MinItemDurability[0], ignoreInWater=item.bDurabilityRequirementIgnoredInWater[0])
 
         v['crafting'] = dict(
-            xp = item.BaseCraftingXP[0],
+            xp=item.BaseCraftingXP[0],
             bpCraftTime=(item.MinBlueprintTimeToCraft[0], item.BlueprintTimeToCraft[0]),
             minLevelReq=item.CraftingMinLevelRequirement[0],
-            productCount=item.CraftingGiveItemCount[0]
+            productCount=item.CraftingGiveItemCount[0],
         )
+
         if item.bAllowRepair[0]:
             v['repair'] = dict(
                 xp=item.BaseRepairingXP[0],
                 time=item.TimeForFullRepair[0],
-                resourceMult=item.RepairResourceRequirementMultiplier[0]
+                resourceMult=item.RepairResourceRequirementMultiplier[0],
             )
 
         if 'StructureToBuild' in item and item.StructureToBuild[0].value.value:
@@ -93,10 +89,15 @@ class ItemsStage(JsonHierarchyExportStage):
             if recipe.values:
                 v['crafting']['recipe'] = [convert_recipe_entry(entry.as_dict()) for entry in recipe.values]
 
-        if item.has_override('OverrideRepairingRequirements'):
+        if 'OverrideRepairingRequirements' in item:
             recipe = item.OverrideRepairingRequirements[0]
             if recipe.values:
+                v['repair'] = dict()
                 v['repair']['recipe'] = [convert_recipe_entry(entry.as_dict()) for entry in recipe.values]
+
+        if item.has_override('UseItemAddCharacterStatusValues'):
+            status_effects = item.UseItemAddCharacterStatusValues[0]
+            v['statEffects'] = dict(convert_status_effect(entry) for entry in status_effects.values)
 
         return v
 
@@ -108,3 +109,49 @@ def convert_recipe_entry(entry):
         type=entry['ResourceItemType'].value.value.name,
     )
     return result
+
+
+def convert_status_effect(entry):
+    d = entry.as_dict()
+    stat_name = d['StatusValueType'].get_enum_value_name().lower()
+    result = dict(
+        value=d['BaseAmountToAdd'],
+        useItemQuality=d['bUseItemQuality'],
+        descriptionIndex=d['StatusValueModifierDescriptionIndex'],
+
+        # 'bContinueOnUnchangedValue = (BoolProperty) False',
+        # 'bResetExistingModifierDescriptionIndex = (BoolProperty) False',
+        # 'LimitExistingModifierDescriptionToMaxAmount = (FloatProperty) 0.0',
+        # 'ItemQualityAddValueMultiplier = (FloatProperty) 1.0',
+        # 'StopAtValueNearMax = (ByteProperty) ByteProperty(EPrimalCharacterStatusValue, EPrimalCharacterStatusValue::MAX)',
+        # 'ScaleValueByCharacterDamageType = (ObjectProperty) None'),
+    )
+
+    pctOfMax = d['bPercentOfMaxStatusValue']
+    pctOfCur = d['bPercentOfCurrentStatusValue']
+    if pctOfCur or pctOfMax:
+        result['pctOf'] = 'max' if pctOfMax else 'current'
+        result['pctAbsRange'] = (d['PercentAbsoluteMinValue'], d['PercentAbsoluteMaxValue'])
+
+    if d['bSetValue']: result['setValue'] = True
+    if d['bSetAdditionalValue']: result['setAddValue'] = True
+    if d['bForceUseStatOnDinos']: result['forceUseOnDino'] = True
+    if not d['bDontRequireLessThanMaxToUse']: result['allowWhenFull'] = False
+
+    qlyMult = d['ItemQualityAddValueMultiplier']
+    if qlyMult != 1.0:
+        result['qualityMult'] = d['ItemQualityAddValueMultiplier']
+
+    if d['bAddOverTimeSpeedInSeconds']:
+        result['duration'] = d['AddOverTimeSpeed']
+    else:
+        if d['AddOverTimeSpeed'] == 0:
+            # Assume instant
+            result['duration'] = 0
+        else:
+            # Duration = amount / speed
+            duration = abs(d['BaseAmountToAdd']) / d['AddOverTimeSpeed']
+            duration = float(format(duration, '.7g'))
+            result['duration'] = duration
+
+    return (stat_name, result)
