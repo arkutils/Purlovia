069526cd798ae615a39ab425815e26a194b10544
alex4401 <rylatgl@gmail.com>
2020-02-10 09:47:31 +0100

    Clean linting errors in spawning group extraction

--- export/wiki/stage_spawn_groups.py
+++ export/wiki/stage_spawn_groups.py
@@ -113,13 +113,16 @@ def convert_class_swaps(pgd: UAsset):
     return all_values
 
 def segregate_container_additions(pgd: UAsset):
+    if not pgd.default_export:
+        return None
+        
     export_data = pgd.default_export.properties
     d = export_data.get_property('TheNPCSpawnEntriesContainerAdditions', fallback=None)
     if not d:
         return None
     
     # Extract the addition entries
-    change_queues = dict()
+    change_queues: Dict[str, List[Dict[str, Any]]] = dict()
     for add in d.values:
         add = add.as_dict()
         klass = add['SpawnEntriesContainerClass']
