6e959c4c8cb9e30faa31c9791d05cc4ed1036de4
coldino <antoranthony.purchase@gmail.com>
2020-03-29 16:10:38 +0100

    Extend perf and memory testing

---
+++ utils/measuremem.py
@@ -0,0 +1,58 @@
+'''
+A utility to measure the memory usage of a Python snippet, in a style similar to `timeit`.
+
+Usage: measuremem.py <setup snippet> <measured snippet>
+
+(remember to use quotes to encapsulate each statememt)
+'''
+
+import os
+import sys
+from typing import Any, Dict, List
+
+from guppy import hpy
+
+SRC_NAME = '<cmdline>'
+
+TMPL = '''
+def inner():
+    {setup}
+    hp = hpy()
+    hp.setrelheap()
+    {article}
+    h = hp.heap()
+
+    return h.size
+'''
+
+
+def main(args: List[str]) -> int:
+    if len(args) != 3:
+        print("Usage: measuremem.py <setup snippet> <measured snippet>", file=sys.stderr)
+        sys.exit(1)
+
+    setup_snippet = args[1] or 'pass'
+    article_snippet = args[2]
+
+    sys.path.insert(0, os.curdir)
+
+    # Verify they each compile
+    compile(setup_snippet, SRC_NAME, "exec")
+    compile(article_snippet, SRC_NAME, "exec")
+
+    # Make up the runnable snippet
+    src = TMPL.format(setup=setup_snippet, article=article_snippet)
+    code = compile(src, SRC_NAME, "exec")
+    local_ns: Dict[str, Any] = dict()
+    exec(code, globals(), local_ns)  # noqa  # pylint: disable=exec-used
+    inner_fn = local_ns['inner']
+
+    # Run it and display the result
+    heap = inner_fn()
+    print(heap)
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
