aa8b6b0b8d7af4589dbbb0ce23ae3b4c5b44990f
VolatilePulse <shearer.zachary@gmail.com>
2020-04-11 02:15:30 -0400

    Slightly optimize ASB Stats code

--- export/asb/stats.py
+++ export/asb/stats.py
@@ -14,32 +14,34 @@ IS_PERCENT_STAT = (0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1)
 def gather_stat_data(dcsc_props: PrimalDinoStatusComponent, statIndexes: Tuple[int, ...]) -> List[Optional[List[float]]]:
     statsArray = list()
 
-    iw_values: List[float] = list()
+    iw_values: List[float] = [0] * len(statIndexes)
     for _, ark_index in enumerate(statIndexes):
         can_level = (ark_index == 2) or dcsc_props.CanLevelUpValue[ark_index]
-        add_one = 1 if IS_PERCENT_STAT[ark_index] else 0
-        zero_mult = 1 if can_level else 0
-        ETHM = dcsc_props.ExtraTamedHealthMultiplier[0].rounded_value if ark_index == 0 else 1
-
-        # Overrides the IW value for Torpor. While this hasn't been seen before, a species may allow torpor
-        #   to be leveled in the wild. Unsure how Ark would handle this.
-        if ark_index == 2:
-            iw_values.append(dcsc_props.TheMaxTorporIncreasePerBaseLevel[0].rounded_value)
-        else:
-            iw_values.append(dcsc_props.AmountMaxGainedPerLevelUpValue[ark_index].rounded_value)
-
-        stat_data: Optional[List[float]] = [
-            cd(dcsc_props.MaxStatusValues[ark_index].rounded_value + add_one),
-            cd(iw_values[ark_index] * zero_mult),
-            cd(dcsc_props.AmountMaxGainedPerLevelUpValueTamed[ark_index].rounded_value * ETHM * zero_mult),
-            cf(dcsc_props.TamingMaxStatAdditions[ark_index].rounded_value),
-            cf(dcsc_props.TamingMaxStatMultipliers[ark_index].rounded_value),
-        ]
+        dont_use = dcsc_props.DontUseValue[ark_index]
 
         # Creates a null value in the JSON for stats that are unused
-        dont_use = dcsc_props.DontUseValue[ark_index]
         if dont_use and not can_level:
-            stat_data = None
+            stat_data: Optional[List[float]] = None
+
+        else:
+            add_one = 1 if IS_PERCENT_STAT[ark_index] else 0
+            zero_mult = 1 if can_level else 0
+            ETHM = dcsc_props.ExtraTamedHealthMultiplier[0].rounded_value if ark_index == 0 else 1
+
+            # Overrides the IW value for Torpor. While this hasn't been seen before, a species may allow torpor
+            #   to be leveled in the wild. Unsure how Ark would handle this.
+            if ark_index == 2:
+                iw_values[ark_index] = dcsc_props.TheMaxTorporIncreasePerBaseLevel[0].rounded_value
+            else:
+                iw_values[ark_index] = dcsc_props.AmountMaxGainedPerLevelUpValue[ark_index].rounded_value
+
+            stat_data = [
+                cd(dcsc_props.MaxStatusValues[ark_index].rounded_value + add_one),
+                cd(iw_values[ark_index] * zero_mult),
+                cd(dcsc_props.AmountMaxGainedPerLevelUpValueTamed[ark_index].rounded_value * ETHM * zero_mult),
+                cf(dcsc_props.TamingMaxStatAdditions[ark_index].rounded_value),
+                cf(dcsc_props.TamingMaxStatMultipliers[ark_index].rounded_value),
+            ]
 
         statsArray.append(stat_data)
 
