cd25063c5743c1ee182a54e598be0be96126b476
alex4401 <rylatgl@gmail.com>
2020-04-27 12:01:24 +0200

    Bump format versions of exports affected by min/max/pow change

--- export/wiki/stage_loot_crates.py
+++ export/wiki/stage_loot_crates.py
@@ -12,13 +12,12 @@ __all__ = [
     'LootCratesStage',
 ]
 
-
 logger = get_logger(__name__)
 
 
 class LootCratesStage(JsonHierarchyExportStage):
     def get_format_version(self) -> str:
-        return "2"
+        return "3"
 
     def get_name(self) -> str:
         return "loot_crates"
