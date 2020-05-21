4d06a1e9484baade3b4b0641b2982785f110d2e2
coldino <antoranthony.purchase@gmail.com>
2020-05-21 20:13:35 +0100

    Add collecting taming item overides for species

--- ark/taming.py
+++ ark/taming.py
@@ -1,7 +1,9 @@
 '''Parse and cache taming food data for use in extraction phases.'''
 
 from dataclasses import dataclass, field
-from typing import Optional, Tuple
+from functools import lru_cache
+from operator import itemgetter
+from typing import Any, Dict, List, Optional, Set, Tuple
 
 from ue.gathering import gather_properties
 from ue.hierarchy import find_parent_classes, find_sub_classes
@@ -10,7 +12,7 @@ from ue.properties import StructProperty
 from ue.tree import is_fullname_an_asset
 from utils.tree import IndexedTree
 
-from .types import PrimalItem
+from .types import PrimalDinoCharacter, PrimalDinoSettings, PrimalItem
 
 __all__ = [
     'TamingFoodHandler',
@@ -26,6 +28,16 @@ class TamingFoodHandler:
 class ItemStatEffect:
     base: float
     speed: Optional[float] = None
+
+
+@dataclass(init=False)
+class ItemOverride:
+    bp: str
+    priority: float
+    food_mult: float
+    torpor_mult: float
+    affinity_mult: float
+    affinity_override: float
 
 
 @dataclass
@@ -46,24 +58,80 @@ items: IndexedTree[Item] = IndexedTree(
 def _gather_items(loader: AssetLoader):
     items.clear()
 
-    for clsname in find_sub_classes(PrimalItem.get_ue_type()):
-        if clsname.startswith('/Game/Mods/'): continue  # REMOVE ME!!!
+    for cls_name in find_sub_classes(PrimalItem.get_ue_type()):
+        if cls_name.startswith('/Game/Mods/'): continue  # REMOVE ME!!!
 
-        item = Item(bp=clsname)
-        parent = _get_parent(clsname)
+        item = Item(bp=cls_name)
+        parent = _get_parent(cls_name)
         items.add(parent, item)
 
-        if not is_fullname_an_asset(clsname): continue
+        if not is_fullname_an_asset(cls_name): continue
 
-        asset = loader[clsname]
-        assert asset.default_export
-        proxy: PrimalItem = gather_properties(asset.default_export)
+        asset = loader[cls_name]
+        proxy: PrimalItem = gather_properties(asset)
         item.name = str(proxy.DescriptiveNameBase[0])
         _collect_item_effects(item, proxy)
 
 
-def _get_parent(clsname: str) -> str:
-    parent = next(find_parent_classes(clsname, include_self=False))
+def _collect_species_data(cls_name: str, loader: AssetLoader) -> List[ItemOverride]:
+    settings_cls = _get_species_settings_cls(cls_name, loader)
+    if not settings_cls:
+        return []
+    foods = _collect_settings_foods(settings_cls, loader)
+    return foods
+
+
+def _get_species_settings_cls(cls_name: str, loader: AssetLoader) -> Optional[str]:
+    asset = loader[cls_name]
+    proxy: PrimalDinoCharacter = gather_properties(asset)
+    settings = proxy.get('DinoSettingsClass', 0, fallback=None)
+    if settings is None:
+        return None
+    return settings.value.value.fullname
+
+
+@lru_cache(maxsize=100)
+def _collect_settings_foods(cls_name: str, loader: AssetLoader) -> List[ItemOverride]:
+    asset = loader[cls_name]
+    proxy: PrimalDinoSettings = gather_properties(asset)
+
+    # Join base and extra entries
+    normal_list = proxy.get('FoodEffectivenessMultipliers', 0, None)
+    extra_list = proxy.get('ExtraFoodEffectivenessMultipliers', 0, None)
+    foods: List[ItemOverride] = []
+    if normal_list:
+        foods.extend(_collect_settings_effect(food) for food in normal_list.values)
+    if extra_list:
+        foods.extend(_collect_settings_effect(food) for food in extra_list.values)
+
+    # Remove duplicates, in reverse # TODO: Verify priority of duplicates
+    bps: Set[str] = set()
+    foods.reverse()
+    for i, food in enumerate(foods):
+        if food.bp in bps:
+            del foods[i]
+
+        bps.add(food.bp)
+
+    foods.reverse()
+
+    return foods
+
+
+def _collect_settings_effect(food: StructProperty) -> ItemOverride:
+    v = food.as_dict()
+    o = ItemOverride()
+    o.bp = v['FoodItemParent'].value.value.fullname
+    o.priority = float(v['UntamedFoodConsumptionPriority'])
+    o.food_mult = float(v['FoodEffectivenessMultiplier'])
+    o.torpor_mult = float(v['TorpidityEffectivenessMultiplier'])
+    o.affinity_mult = float(v['AffinityEffectivenessMultiplier'])
+    o.affinity_override = float(v['AffinityOverride'])
+    return o
+
+
+def _get_parent(cls_name: str) -> str:
+    parent = next(find_parent_classes(cls_name, include_self=False))
     return parent
 
 
@@ -89,3 +157,25 @@ def _collect_status_effects(effect: StructProperty) -> ItemStatEffect:
     else:
         o.speed = None
     return o
+
+
+def print_species_effects(cls_name: str, loader: AssetLoader):
+    foods = _collect_species_data(cls_name, loader)
+    data: List[Dict[str, Any]] = [vars(food) for food in foods]
+    for food in data:
+        bp = food['bp']
+        bp = bp[bp.rfind('.') + 1:]
+        bp = bp.replace('PrimalItemConsumable_', '')
+        if bp.endswith('_C'): bp = bp[:-2]
+        food['bp'] = bp
+
+    headers = ('classname', 'pri', 'food*', 'torp*', 'aff*', 'aff=')
+
+    def affinity_calc(food: Dict[str, Any]):
+        return food['affinity_override']  # * food['affinity_mult']
+
+    values = [food.values() for food in sorted(data, reverse=True, key=affinity_calc)]
+
+    from tabulate import tabulate
+    table = tabulate([headers, *values], headers='firstrow')
+    print(table)
