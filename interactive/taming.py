265bba000c8a2485239b99ecba48c5d441c12ec7
coldino <antoranthony.purchase@gmail.com>
2020-07-12 10:15:06 +0100

    Add interactive taming file

---
+++ interactive/taming.py
@@ -0,0 +1,7 @@
+from tests.common import *
+
+from ark.taming_food.calc import *
+from ark.taming_food.datatypes import *
+from ark.taming_food.debug import *
+from ark.taming_food.items import *
+from ark.taming_food.species import *
