4afc93b218108092d12da56886a9d45065db697d
coldino <antoranthony.purchase@gmail.com>
2021-01-29 14:43:01 +0000

    Fix a formatting issue

--- interactive/taming.py
+++ interactive/taming.py
@@ -1,7 +1,6 @@
-from tests.common import *
-
 from ark.taming_food.calc import *
 from ark.taming_food.datatypes import *
 from ark.taming_food.debug import *
 from ark.taming_food.items import *
 from ark.taming_food.species import *
+from tests.common import *
