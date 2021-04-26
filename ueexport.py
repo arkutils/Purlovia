79804bc0a8246222eb5b2d08d3ebfbe4fb07162f
alex4401 <rylatgl@gmail.com>
2021-04-27 01:25:44 +0200

    Extend ueexport to export item icon info for Oviraptor renders

--- ueexport.py
+++ ueexport.py
@@ -3,12 +3,19 @@ import sys
 from contextlib import contextmanager
 from pathlib import Path
 from pprint import pprint
-from typing import Any, Optional, Tuple
+from typing import Any, Dict, Optional, Tuple, cast
 
+from ark.discovery import initialise_hierarchy
+from ark.types import PrimalItem
 from automate.ark import ArkSteamManager
 from automate.jsonutils import _format_json
 from browseasset import find_asset
-from ue.asset import UAsset
+from config import get_global_config
+from export.wiki.stage_items import get_item_name
+from ue.asset import ExportTableItem, UAsset
+from ue.gathering import gather_properties
+from ue.hierarchy import inherits_from
+from ue.proxy import UEProxyStructure
 from ue.utils import sanitise_output
 
 EPILOG = '''example: python ueexport.py /Game/PrimalEarth/Dinos/Dodo/Dodo_Character_BP'''
@@ -25,6 +32,7 @@ def create_parser() -> argparse.ArgumentParser:
     parser.add_argument('--output', '-o', nargs=1, help='output to a specific file')
     parser.add_argument('--all', '-a', action='store_true', help='include all exports')
     parser.add_argument('--default', '-d', action='store_true', help='include only the default export')
+    parser.add_argument('--ovi', action='store_true', help='create a 2D item icon render template for Oviraptor')
     parser.add_argument('assetname', metavar='ASSETNAME', type=str, help="asset to convert")
     parser.add_argument('export',
                         metavar='EXPORT',
@@ -36,7 +44,17 @@ def create_parser() -> argparse.ArgumentParser:
 
 
 def collect_asset(assetname: str) -> UAsset:
-    arkman = ArkSteamManager()
+    config = get_global_config()
+    config.settings.SkipInstall = True
+    arkman = ArkSteamManager(config=config)
+
+    if args.ovi:
+        # Hierarchy is needed for proxies in the Oviraptor target.
+        arkman.ensureSteamCmd()
+        arkman.ensureGameUpdated()
+        arkman.ensureModsUpdated(config.mods)
+        initialise_hierarchy(arkman, config)
+
     loader = arkman.getLoader()
     # config = get_global_config()
 
@@ -58,11 +76,7 @@ def create_filename(name: str) -> str:
     return name
 
 
-def collect_data(asset: UAsset) -> Tuple[str, Any]:
-    if args.default and args.export is not None:
-        print("Cannot specify an export with --default", file=sys.stderr)
-        sys.exit(1)
-
+def find_export(asset: UAsset) -> Optional[ExportTableItem]:
     if args.default:
         # Produce default export only
         if not hasattr(asset, 'default_export'):
@@ -70,8 +84,7 @@ def collect_data(asset: UAsset) -> Tuple[str, Any]:
             sys.exit(1)
 
         assert asset.default_export and asset.default_export.fullname
-        data = sanitise_output(asset.default_export.properties)
-        filename = create_filename(asset.default_export.fullname)
+        return asset.default_export
 
     elif args.export:
         # Single export
@@ -87,7 +100,7 @@ def collect_data(asset: UAsset) -> Tuple[str, Any]:
                 print(f"Maximum export index for this asset is {len(asset.exports.values)-1}", file=sys.stderr)
                 sys.exit(1)
 
-            export = asset.exports[as_int]
+            return asset.exports[as_int]
         else:
             # Name-based export lookup
             found_indexes = []
@@ -107,10 +120,81 @@ def collect_data(asset: UAsset) -> Tuple[str, Any]:
                 pprint(found_indexes, stream=sys.stderr)
                 sys.exit(1)
 
-            export = asset.exports.values[found_indexes[0]]
+            return asset.exports.values[found_indexes[0]]
 
+    return None
+
+
+def collect_data(asset: UAsset) -> Tuple[str, Any]:
+    if args.default and args.export is not None:
+        print("Cannot specify an export with --default", file=sys.stderr)
+        sys.exit(1)
+
+    if args.ovi:
+        if not args.export and not args.default:
+            # Grab the default export since we need a starting point for the proxy.
+            args.default = True
+        export = find_export(asset)
+        assert export
+        assert export.fullname
+
+        if not inherits_from(export, PrimalItem.get_ue_type()):
+            print(f"Export {export.name} is not a subclass of PrimalItem.", file=sys.stderr)
+            sys.exit(1)
+
+        proxy: UEProxyStructure = gather_properties(export)
+        item = cast(PrimalItem, proxy)
+
+        if 'ItemIconMaterialParent' not in item:
+            print("Item does not use an icon shader", file=sys.stderr)
+            sys.exit(1)
+
+        name = get_item_name(item) or export.name
+        data: Dict[str, Any] = dict(
+            Format='2.0',
+            Name=name,
+            BlueprintPath=export.fullname,
+        )
+
+        assert asset.loader
+        mat_instance = asset.loader.load_related(item.ItemIconMaterialParent[0]).default_export
+        assert mat_instance
+        mat_properties = mat_instance.properties.as_dict()
+
+        # Convert all parameters from the material instance.
+        parameters = dict()
+        if 'ScalarParameterValues' in mat_properties:
+            for param in mat_properties['ScalarParameterValues'][0].values:
+                param_info = param.as_dict()
+                param_name = sanitise_output(param_info['ParameterName'])
+                parameters[param_name] = param_info['ParameterValue']
+        if 'VectorParameterValues' in mat_properties:
+            for param in mat_properties['VectorParameterValues'][0].values:
+                param_info = param.as_dict()
+                param_name = sanitise_output(param_info['ParameterName'])
+                parameters[param_name] = param_info['ParameterValue'].values[0]
+        if 'TextureParameterValues' in mat_properties:
+            for param in mat_properties['TextureParameterValues'][0].values:
+                param_info = param.as_dict()
+                param_name = sanitise_output(param_info['ParameterName'])
+                parameters[param_name] = param_info['ParameterValue']
+
+        if parameters:
+            data['2DMaterial'] = dict(Parent=mat_properties['Parent'][0], **parameters)
+        else:
+            # Export only the parent material as the instance has no parameters.
+            data['2DMaterial'] = mat_properties['Parent'][0]
+
+        data = sanitise_output(data)
+        filename = create_filename(export.fullname)
+
+    elif args.default or args.export:
+        export = find_export(asset)
+        assert export
+        assert export.fullname
         data = sanitise_output(export.properties)
         filename = create_filename(export.fullname)
+
     else:
         # Full asset extraction
         data = sanitise_output(asset)
