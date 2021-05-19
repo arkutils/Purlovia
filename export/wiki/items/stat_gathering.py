0f9053c88e380755acfa9427d71578ff7a7cdb50
alex4401 <rylatgl@gmail.com>
2021-05-19 21:22:20 +0200

    Generalize wiki.items' inherited struct field gathering

--- export/wiki/items/stat_gathering.py
+++ export/wiki/items/stat_gathering.py
@@ -2,8 +2,7 @@ from enum import Enum
 from typing import Any, Dict
 
 from ark.types import PrimalItem
-from ue.asset import ExportTableItem
-from ue.hierarchy import find_parent_classes
+from export.wiki.inherited_structs import gather_inherited_struct_fields
 
 DEFAULTS = {
     # DevKit Verified
@@ -26,34 +25,7 @@ class Stat(Enum):
     HyperInsulation = 7
 
 
-def _gather_props_from_export(export: ExportTableItem, index: int, output: Dict[str, Any]):
-    props = export.properties.get_property('ItemStatInfos', index, fallback=None)
-    if not props:
-        return
-
-    for key, value in props.as_dict().items():
-        output[key] = value
-
-
 def gather_item_stat(item: PrimalItem, index: Stat) -> Dict[str, Any]:
     leaf_export = item.get_source()
-    loader = leaf_export.asset.loader
-    output = dict(DEFAULTS)
     ark_index = index.value
-
-    # Make sure this is always the default export, and not e.g. default class.
-    leaf_export = leaf_export.asset.default_export
-
-    for kls in reversed(list(find_parent_classes(leaf_export))):
-
-        if not kls.startswith('/Game'):
-            continue
-
-        # Load the asset and get its default export (the CDO).
-        # find_parent_classes gives us merely a list of parent classes, and not "parent" CDOs.
-        asset = loader[kls]
-        super_export = asset.default_export
-
-        _gather_props_from_export(super_export, ark_index, output)
-
-    return output
+    return gather_inherited_struct_fields(leaf_export, 'ItemStatInfos', DEFAULTS, ark_index)
