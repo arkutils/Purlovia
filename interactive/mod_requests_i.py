4b01f2a548ec75dd600d84ddef58d150d2dcacf3
coldino <antoranthony.purchase@gmail.com>
2022-01-16 20:19:24 +0000

    Remove added mods from mod_requests_i.py

--- interactive/mod_requests_i.py
+++ interactive/mod_requests_i.py
@@ -30,10 +30,10 @@ loader = arkman.getLoader()
 
 modids = set([
     # Delayed decisions
-    '538986229',  # Annunaki Genesis - old but still loved and kind of working
     '916417001',  # MAP: Ebenus Astrum -                                        CHECK for dinos
 
     # No
+    # '538986229',   # Annunaki Genesis - obsolete, replaced with Ark Creatures Rebalanced (AG REBORN)
     # '972887420',   # Jurassic Park Expansion - massive and discontinued
     # '2205699189',  # Alpha Everything - probably only buffs
     # '2016338122',  # Shiny! Dinos - uses buffs
@@ -46,12 +46,10 @@ modids = set([
 
     # Candidates
     '843960973',  # Dragontail
-    '1188224480',  # Tamable Alphas by Ogdaonly
     '1679826889',  # Caballus Custom Map - The Equestrian Lan
     '1754846792',  # Zytharian Critters
     '1880357240',  # Sanctuary Overhaul
     '1989252120',  # ARK: Reclamation
-    '1169020368',  # Ark Creatures Rebalanced (AG REBORN) 2.0
     '1558114752',  # Dino hybrids & more (Radioactive tag?)
     '1967035140',  # AramoorePlus
     '2212177129',  # Sid's Hybrids
@@ -59,23 +57,16 @@ modids = set([
     '1840936777',  # Prometheus
     '1511268523',  # Gunsmoke Animals
     '2251437896',  # TheBurningLoop Community Mod
-    '1139775728',  # Confuciusornis
     '908817184',  # Nightmare Pegasus
-    '2337840412 ',  # RR StarFarmanimals
-    '2360410335 ',  # RR StarExoticAnimals
-    '1783308979',  # Additional Dinos Valguero
-    '1824174926',  # Carnotaurus TLC
+    '2337840412',  # RR StarFarmanimals
+    '2360410335',  # RR StarExoticAnimals
     '1592652278',  # Random Egg Spawner 2.0
     '710880648',  # DinoOverhaul X
     '1416912482',  # Bunn3h's extras
     '2117433951',  # Galvanized Wolf
-    '883957187',  # Wyvern World
     '2472371628',  # MilicrocaWarriors CommunityMod
     '1534108504',  # VWSR - Dino Rebalance, Tameable Alphas, Sub Species and More
-    '2003934830',  # Prehistoric Beasts
     '1852495701',  # Shad's Better Gigas Rebalance
-    '1178308359',  # Shadlos's Tameable Bosses
-    '2447186973',  # Ark Omega
     '1134797219',  # Breedable Griffins
     '1838617463',  # Fjordur
 ])
@@ -104,16 +95,17 @@ def data_from_mod(data):
         raise ex
 
 
+#%% Check for mods already enabled
+already_enabled = set(config.mods).intersection(modids)
+if already_enabled:
+    print('Already in config:')
+    for modid in already_enabled:
+        print(modid)
+    print()
+
 #%% Fetch data for them all
 mod_data = SteamApi.GetPublishedFileDetails(list(modids))
 output_data = [data_from_mod(d) for d in mod_data]
-
-#%% CSV export
-
-with open('livedata/mod_requests.csv', 'wt', newline='', encoding='utf-8') as f:
-    csv_writer = csv.writer(f)
-    csv_writer.writerow(output_data[0].keys())
-    csv_writer.writerows(mod.values() for mod in output_data)
 
 #%% Config addition
 
@@ -127,3 +119,13 @@ for mod in sorted(output_data, key=lambda v: int(v['id'])):
     title = mod['title'][:40].replace('"', '\\"').replace('\n', ' ')
     title = f'"{title}"'
     print(f"{mod['id']:<10} = {title:42} # {comment}")
+
+#%% CSV export
+
+csv_filename = 'livedata/mod_requests.csv'
+with open(csv_filename, 'wt', newline='', encoding='utf-8') as f:
+    csv_writer = csv.writer(f)
+    csv_writer.writerow(output_data[0].keys())
+    csv_writer.writerows(mod.values() for mod in output_data)
+    print()
+    print(f'CSV saved as: {csv_filename}')
