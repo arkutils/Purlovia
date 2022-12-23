ece696fe08f432d6621294e2517108ca4a1c83f0
coldino <antoranthony.purchase@gmail.com>
2022-12-23 21:36:17 +0000

    Update run manager

--- manage/run.py
+++ manage/run.py
@@ -14,8 +14,8 @@ def generate_run_command(run: Run, args: list[str]) -> list[str]:
     if run.stages is not None:
         command.append(",".join(run.stages))
 
-    if run.appid is not None:
-        command.append(f'--appid={run.appid}')
+    if run.config is not None:
+        command.append(f'--config-file={run.config}')
 
     if run.include_official_mods:
         command.append('--include-official-mods')
@@ -25,9 +25,6 @@ def generate_run_command(run: Run, args: list[str]) -> list[str]:
 
     if run.maps is not None:
         command.append(f'--maps={",".join(run.maps)}')
-
-    if run.output_path:
-        command.append(f'--output-path={run.output_path}')
 
     if args:
         command.extend(args)
